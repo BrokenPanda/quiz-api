ee981f032fb507f5e5c4103b23f3c97b8c4840bf
BrokenPanda <brokenpanda@protonmail.com>
2022-09-17 11:04:48 +0200

    added dependencies and edited package.json

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express = require ('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/quiz');
 
+const helmet = require('helmet');
+const compression = require('compression');
 const app = express();
 
 app.use(express.json());
-
 app.use('/', routes);
+app.use(helmet());
+app.use(compression());
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('App is listening on port ' + listener.address().port);
